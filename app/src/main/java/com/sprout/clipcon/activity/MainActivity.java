2a4a042ecd34772006befac06be32eee788bb22b
doooyeon <ehdus5958@naver.com>
2017-09-25 17:37:40 +0900

    Check external storage write permission at application startup

--- app/src/main/java/com/sprout/clipcon/activity/MainActivity.java
+++ app/src/main/java/com/sprout/clipcon/activity/MainActivity.java
@@ -1,13 +1,20 @@
 package com.sprout.clipcon.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v13.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.sprout.clipcon.R;
@@ -18,6 +25,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int MY_PERMISSIONS_REQUEST_WRITE_STORAGE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
                 showJoinDialog();
             }
         });
+
+        writeExternalStoragePrmissionCheck();
     }
 
     public void showJoinDialog() {
@@ -102,5 +113,36 @@ public class MainActivity extends AppCompatActivity {
                 .content(R.string.checkKey)
                 .positiveText(R.string.confirm)
                 .show();
+    }
+
+    public void writeExternalStoragePrmissionCheck() {
+        /* 사용자의 OS 버전이 마시멜로우 이상인지 체크한다. */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            /* 사용자 단말기의 권한 중 "저장소 쓰기" 권한이 허용되어 있는지 체크한다.
+             */
+            int permissionResult = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+            /* WRITE_EXTERNAL_STORAGE의 권한이 없을 때 */
+            if (permissionResult == PackageManager.PERMISSION_DENIED) {
+                /* 사용자가 WRITE_EXTERNAL_STORAGE의 권한을 한번이라도 거부한 적이 있는 지 조사한다.
+                 * 거부한 이력이 한번이라도 있다면, true를 리턴한다.
+                 */
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_STORAGE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    ActivityCompat.finishAffinity(this);
+                    System.exit(0);
+                }
+                return;
+            }
+        }
     }
 }
